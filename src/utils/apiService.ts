ccdd3482d913b5c75d778316496eb897a3cacad2
teotsi <teotsi@gmail.com>
2021-04-14 01:33:01 +0300

    add error handling for requests

--- src/utils/apiService.ts
+++ src/utils/apiService.ts
@@ -4,6 +4,15 @@ const ApiService = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL
 });
 
-export const getAllPointsOfInterest = () => ApiService.get('poi').then((res) => res.data);
+export const getAllPointsOfInterest = () => ApiService
+    .get('poi')
+    .then((res) => res.data)
+    .catch(err => {
+        console.log(err);
+        return []
+    });
 
-export const registerPointOfInterest = (newPoi : PointOfInterest) => ApiService.post('poi', newPoi).then((res) => res.data);
+export const registerPointOfInterest = (newPoi: PointOfInterest) => ApiService
+    .post('poi', newPoi)
+    .then((res) => res.data)
+    .catch(err => console.log(err));
