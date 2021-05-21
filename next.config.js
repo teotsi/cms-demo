76faa58bb81960662ec90c61c4963b6eaba9c463
teotsi <teotsi@gmail.com>
2021-05-21 18:46:50 +0300

    update webpack paths

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+path = require("path");
+
 module.exports = {
   future: {
     webpack5: true,
   },
   webpack(config) {
+
+    config.resolve.alias["@app"] = path.resolve(__dirname, "./src"),
+    config.resolve.alias["@components"] = path.resolve(__dirname, "./src/components"),
+    config.resolve.alias["@icons"] = path.resolve(__dirname, "./src/assets/icons"),
+    config.resolve.alias["@utils"] = path.resolve(__dirname, "./src/utils"),
+    config.resolve.alias["@zustand"] = path.resolve(__dirname, "./src/zustand"),
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"]
