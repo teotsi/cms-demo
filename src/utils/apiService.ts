3e8f009c60ef8b5d596a6ade806beffd358ee3e6
teotsi <teotsi@gmail.com>
2021-05-21 18:17:00 +0300

    add access-point endpoints

--- src/utils/apiService.ts
+++ src/utils/apiService.ts
@@ -4,7 +4,7 @@ const ApiService = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL
 });
 
-export const getAllPointsOfInterest = () => ApiService
+export const getAllPointsOfInterest = (): Promise<PointOfInterest[]> => ApiService
     .get('poi')
     .then((res) => res.data)
     .catch(err => {
@@ -14,5 +14,18 @@ export const getAllPointsOfInterest = () => ApiService
 
 export const registerPointOfInterest = (newPoi: PointOfInterest) => ApiService
     .post('poi', newPoi)
+    .then((res) => res.data)
+    .catch(err => console.log(err));
+
+export const getAllAccessPoints = (): Promise<AccessPoint[]> => ApiService
+    .get('access-point')
+    .then((res) => res.data)
+    .catch(err => {
+        console.log(err);
+        return []
+    });
+
+export const registerAccessPoint = (newAccessPoint: AccessPoint) => ApiService
+    .post('access-point', newAccessPoint)
     .then((res) => res.data)
     .catch(err => console.log(err));
